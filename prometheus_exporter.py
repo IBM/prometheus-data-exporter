7dedf24a8e13894a6dff20c6116912014c993e3d
Riley Zimmerman <rdzimmer@users.noreply.github.com>
2022-02-17 14:02:56 -0500

    Remove copyright header

--- prometheus_exporter.py
+++ prometheus_exporter.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python3
-#-----------------------------------------------------------------------
-# (C) Copyright IBM Corporation 2021.
-#  
-# The source code for this program is not published or otherwise
-# divested of its trade secrets, irrespective of what has
-# been deposited with the U.S. Copyright Office.
-# ----------------------------------------------------------------------
-#
-
 import time
 import requests
 import sys
